50dc938e0b72411a9bc3916d99eccd1250ac811b
Zulkaif Ahmed <zulkaifahmed97@gmail.com>
2022-09-26 03:49:54 +0500

    ping calculator working i guess idk how though

--- pylon/server.js
+++ pylon/server.js
@@ -1,14 +1,50 @@
-const webSocketServer = require('websocket').server;
-const http = require('http');
+var WebSocketServer = require('websocket').server;
+var http = require('http');
 
-const server = http.createServer();
-server.listen(55455);
-const wsServer = new webSocketServer({ httpServer: server });
+var server = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
+});
+server.listen(8080, function() {
+    console.log((new Date()) + ' Server is listening on port 55455');
+});
 
-wsServer.on('request', function (request) {
-    console.log('establishing a new connection with client');
-    var connection = request.accept(null, request.origin);
-    setInterval(() => {
-        connection.sendUTF(new Date().getTime())
-    }, 100);
+wsServer = new WebSocketServer({
+    httpServer: server,
+    // You should not use autoAcceptConnections for production
+    // applications, as it defeats all standard cross-origin protection
+    // facilities built into the protocol and the browser.  You should
+    // *always* verify the connection's origin and decide whether or not
+    // to accept it.
+    autoAcceptConnections: false
+});
+
+function originIsAllowed(origin) {
+  // put logic here to detect whether the specified origin is allowed.
+  return true;
+}
+
+wsServer.on('request', function(request) {
+    if (!originIsAllowed(request.origin)) {
+      // Make sure we only accept requests from an allowed origin
+      request.reject();
+      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+      return;
+    }
+    
+    var connection = request.accept('echo-protocol', request.origin);
+    console.log((new Date()) + ' Connection accepted.');
+    
+    connection.on('message', function(message) {
+        var currdate = new Date(); 
+        
+            console.log('Received Ping at : ' + currdate.getTime()+"was sent at"+ message);
+            connection.sendUTF(parseInt(currdate.getTime()));
+            
+       
+    });
+    connection.on('close', function(reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
 });
